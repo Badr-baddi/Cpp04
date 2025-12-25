e96622089a66629264ec10aa6e2c08cf7e498479
Badr Edinne El Baddi <bael-bad@e3r9p14.1337.ma>
2025-12-25 17:10:23 +0100

    qwerty

---
+++ ex00/Cat.cpp
@@ -0,0 +1,24 @@
+#include "includes/Cat.hpp"
+
+Cat::Cat() : Animal(){
+    std::cout<<" Cat default constructor called "<<std::endl;
+}
+
+Cat::Cat(const Cat &other) : Animal(other){
+    std::cout<<" Cat copy constructor called "<<std::endl;
+}
+
+Cat& Cat::operator=(const Cat &other){
+    if (this != &other)
+        Animal::operator = (other);
+    std::cout<<" Cat copy assignment constructor called "<<std::endl;
+    return *this;
+}
+
+Cat::~Cat(){
+    std::cout<<" Cat destructor called"<<std::endl;
+}
+
+void Cat::makeSound() const{
+    std::cout<<" Meow Meow "<<std::endl;
+}
