a0964d6226cc4b7c76c04c7378ec9605d133b989
Badr Baddi <badrelbaddi727@gmail.com>
2025-12-27 23:16:20 +0100

    finish

--- ex02/Animal.cpp
+++ ex02/Animal.cpp
@@ -22,7 +22,3 @@ Animal& Animal::operator=(const Animal &other){
 std::string Animal::getType() const{
     return this->type;
 }
-
-void Animal::makeSound() const {
-    std::cout << " Animal makes a sound " << std::endl;
-}
